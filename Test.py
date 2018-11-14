5c1bc3f14dbb90b2584030e1a971b8d3fa312090
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-11-14 09:14:11 +0100

    deel 2 opdracht 3

--- Test.py
+++ Test.py
@@ -1,4 +1,14 @@
-def reverse(string):
-    string = string[::-1]
-    return string
-print(reverse('hallo'))
+num = input("Voer een gatal in om te kijken of het een priemgetal is:")
+num = int(num)
+
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is geen priemgetal")
+           print(i,"keer",num//i,"is",num)
+           break
+   else:
+       print(num,"is een priemgetal")
+       
+else:
+   print(num,"is geen priemgetal")
