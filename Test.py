efcacf5d33994b122841fcf30d8b1d45b3bc182e
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-11-22 09:45:38 +0100

    eindopdracht 1.1

--- Test.py
+++ Test.py
@@ -1,12 +1,6 @@
 #boodschappen progamma
 boodschappen = {"brood":2, "water":1, "sap":3}
 
-'''
-print ("Start Len : %d" %  len(boodschappen))
-boodschappen.clear()
-print ("End Len : %d" %  len(boodschappen))
-'''
-
 def print_schermbreedte():
     schermbreedte = 50
     print("="* schermbreedte)
@@ -19,52 +13,51 @@ def main():
     print("Wijzig product, toets 4")
     print("Boodschappen doen, toets 5")
     print("Sla producten op, toets 6")
+    print("Om te stoppen, toets s")
     print_schermbreedte()
-    ant = input("Toets het getal dat je wilt uitvoeren:")
+    ant = input("Toets het getal in dat je wilt uitvoeren:")
 
-    if (ant == '1'):
-        if (boodschappen == ''):
-            print("Geen boodschappen beschikbaar")
+    while ant != 's':
+        if (ant == '1'):
+            if (len(boodschappen) < 0):
+                print("Geen boodschappen beschikbaar")
+            else:
+                overzicht_producten()
+        elif (ant == '2'):
+            add_product()
+        elif (ant == '3'):
+            del_product()
+        elif (ant == '4'):
+            wijzig_product()
+        elif (ant == '5'):
+            winkelen()
+        elif (ant == '6'):
+            opslaan()
         else:
-            overzicht_producten()
-    if (ant == '2'):
-        add_product()
-    if (ant == '3'):
-        del_product()
-    if (ant == '4'):
-        wijzig_product()
-    if (ant == '5'):
-        winkelen()
-    if (ant == '6'):
-        opslaan()
-    else:
-        print("ERROR")
+            print("ERROR")
 
 def overzicht_producten():
     print_schermbreedte()
-    print(boodschappen.keys())
-    print(boodschappen.values())
-    main()
+    for key in boodschappen:
+        print("{key} : {value}".format(key=key, value=boodschappen[key]))
+
 
 def add_product():
     print_schermbreedte()
     key = input("Naam product:")
     value = input("Prijs product:")
     boodschappen[key] = value
-    main()
 
 def del_product():
     print_schermbreedte()
     key = input("Welk product wil je verwijderen?")
     del boodschappen[key]
-    main()
 
 def wijzig_product():
     print_schermbreedte()
     key = input("Wat is de naam van het product?")
     newkey = input("Wat is de nieuwe naam van het product?")
     boodschappen[newkey] = boodschappen.pop(key)
-    main()
 
 def winkelen():
     print_schermbreedte()
@@ -73,8 +66,6 @@ def winkelen():
         product = input("welk product wil je kopen?")
         if (product == 'm'):
             print("Totaal:", prijs, "euro")
-            main()
-            #main()
         if product in boodschappen:
             prijs += boodschappen[product]
 
